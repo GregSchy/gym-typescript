69a5bd0fa49abbe4dd37bcf4cdbc78168d233d67
Grzegorz Szymalak <105386198+GregSchy@users.noreply.github.com>
2023-01-05 18:12:03 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import Navbar from "@/scenes/navbar"
-import { useState } from "react"
-import { SelectedPage } from "@/shared/types"
+import Navbar from "@/scenes/navbar";
+import { useState } from "react";
+import { SelectedPage } from "@/shared/types";
 
 enum SelectedPage {
   Home = "home",
   Benefits = "benefits",
   OurClasses = "ourclasses",
-  ContactUs ="contactus"
+  ContactUs = "contactus",
 }
-
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
 
-  return <div className="app bg-gray-20"> 
-    <Navbar 
-      selectedPage={selectedPage} 
-     setSelectedPage={setSelectedPage} />
-   </div>
+  return (
+    <div className="app bg-gray-20">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+  );
 }
 
-export default App
-
-
+export default App;
